d2c23c8b8c95c7662dffc6eec67d43c180cfc8e0
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-10-23 20:20:23 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alikhan Zhanibek on 2025-10-23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
